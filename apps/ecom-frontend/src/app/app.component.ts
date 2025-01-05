dfe23ca1e2a4d0af00b1cf73f8d0491c3a20b6c2
Woutzah <wouter@blockken.solutions>
2025-01-05 15:03:52 +0100

    Push progress

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { FaConfig, FaIconComponent, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { Oauth2Service } from './auth/oauth2.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   standalone: true,
@@ -15,6 +17,17 @@ import { FooterComponent } from './layout/footer/footer.component';
 export class AppComponent implements OnInit {
   private faIconlibrary = inject(FaIconLibrary);
   private faConfig = inject(FaConfig);
+  private oauth2Service = inject(Oauth2Service);
+
+  private platformId = inject(PLATFORM_ID);
+
+  constructor() {
+    if(isPlatformBrowser(this.platformId)){
+      this.oauth2Service.initAuthentication();
+    }
+
+    this.oauth2Service.connectedUserQuery = this.oauth2Service.fetch()
+  }
 
   ngOnInit() {
     this.initFontAwesome();
--- apps/ecom-frontend/src/environments/environment.development.ts
+++ apps/ecom-frontend/src/environments/environment.development.ts
@@ -1,6 +1,10 @@
 export const environment = {
   kinde: {
     authority: "https://blockkensolutions.kinde.com",
-    // authority: "https://blockkensolutions.kinde.com",
-  }
+    redirectUrl: "http://localhost:4200",
+    postLogoutRedirectUri: "http://localhost:4200",
+    clientId: "0bbed6caef6d411aaff69795ffa8b4e8",
+    audience: "https://blockkensolutions.kinde.com/api",
+  },
+  apiUrl: "http://localhost:8080/api",
 }
